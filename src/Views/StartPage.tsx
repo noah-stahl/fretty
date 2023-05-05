de825f083d1233809bb85b5fda6271f899a993e6
noah-stahl <noah.stahl@mindmatters.de>
2023-05-05 13:05:28 +0200

    Improve routes typing

--- src/Views/StartPage.tsx
+++ src/Views/StartPage.tsx
@@ -1,11 +1,12 @@
 import { Button, Typography } from '@mui/material'
+import { routes } from '../routes'
 
 export function StartPage () {
   return (
     <>
       <Typography variant="h4">Welcome to Fretty!</Typography>
       <Typography variant="subtitle1">A fretboard memorization App</Typography>
-      <Button variant="contained" href="mui">
+      <Button variant="contained" href={routes.fretboard.path}>
         Take Quiz
       </Button>
     </>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,29 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, RouteObject } from 'react-router-dom'
 import { StartPage } from './Views/StartPage'
 import { Fretboard } from './Views/Fretboard'
+import { ErrorPage } from './Views/ErrorPage'
+
+type Route = Pick<RouteObject, 'path' | 'element' | 'errorElement'>
+export const routes = {
+  home: buildRoute({ path: '/', element: <StartPage /> }),
+  fretboard: buildRoute({ path: 'fretboard', element: <Fretboard /> })
+}
+
+function buildRoute ({
+  path,
+  element,
+  errorElement = <ErrorPage />
+}: Route) {
+  return {
+    path,
+    element,
+    errorElement
+  }
+}
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <StartPage />
-  },
-  {
-    path: 'mui',
-    element: <Fretboard />
-  }
+  routes.home,
+  routes.fretboard
 ])
 
 export function Routes () {
